5fcce7344b48db7f85926e4d7b811e9aff514973
wanderingrider <rotarywr250@gmail.com>
2021-03-15 15:00:14 +0900

    新規登録 ビュー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :first_name, :last_name, :read_first_name, :read_last_name])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :nickname
+        end
+      
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+          validates :first_name
+          validates :last_name
+        end
+      
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+          validates :read_first_name
+          validates :read_last_name
+        end
+      
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX
+      
+        has_many :items         
 end
+
