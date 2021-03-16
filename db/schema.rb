621733d11a4f74a4ee5dd86a2090676f2349b378
wanderingrider <rotarywr250@gmail.com>
2021-03-16 11:22:14 +0900

    db・カラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_214541) do
+ActiveRecord::Schema.define(version: 2021_03_16_020147) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "information_name", null: false
+    t.date "theft_day", null: false
+    t.integer "prefecture_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_03_14_214541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
