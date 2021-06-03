610fa879a50bd11c0d3923fb86f9eb26c62a72d7
wanderingrider <rotarywr250@gmail.com>
2021-06-03 18:32:03 +0900

    itemsコントローラー　showテスト

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -33,6 +33,37 @@ describe ItemsController, type: :request do
     end
   end
 
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get item_path(@item)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストすると投稿済の車種名のテキストがある' do
+      get item_path(@item)
+      expect(response.body).to include(@item.information_name)
+    end
+    it 'showアクションにリクエストすると投稿すると投稿済の画像URLがある' do
+      get item_path(@item)
+      expect(response.body).to include('test_image.png')
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿者名の情報がある' do
+      get item_path(@item)
+      expect(response.body).to include(@item.user.nickname)
+    end
+    it 'showアクションにリクエストするとレスポンスとして盗難日の情報がある' do 
+      get item_path(@item)
+      expect(response.body).to include('2021-12-31')
+    end
+    it 'showアクションにリクエストするとレスポンスに盗難にあった場所の情報がある' do
+      get item_path(@item)
+      expect(response.body).to include('2')
+    end
+    it 'showアクションにリクエストするとレスポンスに盗難にあった地域の情報がある' do
+      get item_path(@item)
+      expect(response.body).to include('2')
+    end
+  end
+
 end
 
 
